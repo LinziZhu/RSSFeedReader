e0c6b2b5d5771ae39da728838461801ad3150338
LinziZhu <linzi.zhu.ce@gmail.com>
2017-03-06 23:45:42 +1100

    Reverted Ajax, testing
    Reverted AJAX support, as it has been deprecated, creating some testing
    methods.

--- popup/rss_feed_reader.js
+++ popup/rss_feed_reader.js
@@ -1,14 +1,24 @@
-$.ajax({
-  url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent('http://stackoverflow.com/feeds/question/10943544'),
-  dataType : 'json',
-  success  : function (data) {
-    if (data.responseData.feed && data.responseData.feed.entries) {
-      $.each(data.responseData.feed.entries, function (i, e) {
-        console.log("------------------------");
-        console.log("title      : " + e.title);
-        console.log("author     : " + e.author);
-        console.log("description: " + e.description);
+function getFeed() {
+  $(document).ready(function() {
+    //feed to parse
+    var feed = "http://feeds.feedburner.com/raymondcamdensblog?format=xml";
+
+    $.ajax(feed, {
+        accepts:{
+            xml:"application/rss+xml"
+        },
+        dataType:"xml",
+        success:function(data) {
+            //Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+
+            $(data).find("item").each(function () { // or "item" or whatever suits your feed
+                var el = $(this);
+                console.log("------------------------");
+                console.log("title      : " + el.find("title").text());
+                console.log("link       : " + el.find("link").text());
+                console.log("description: " + el.find("description").text());
+            });
+          }
       });
-    }
-  }
-});
+  });
+}
